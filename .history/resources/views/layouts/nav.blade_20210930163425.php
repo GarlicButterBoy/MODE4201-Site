6a4e4c03ede7faf8a92ead0fe4d1f4e67a1de6ee
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2021-09-30 16:34:54 -0400

    More final touches

--- .history/resources/views/layouts/nav.blade_20210930163425.php
+++ .history/resources/views/layouts/nav.blade_20210930163425.php
@@ -1,5 +1,5 @@
 <div>
-      <h3 class="float-md-start mb-0">MODE4201 - Nick Sturch-Flint</h3>
+      <h3 class="float-md-start mb-0">MODE4201</h3>
       <nav class="nav nav-masthead justify-content-center float-md-end">
         <a class="nav-link active" aria-current="page" href="/">Home</a>
         <a class="nav-link" href="/classworks">Classworks</a>
