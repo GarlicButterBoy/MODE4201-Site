cfefb443e81f41e21453a4ecc71893e2b698095b
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2021-09-30 15:51:47 -0400

    Added youtube link for Part A to lab 2

--- .history/resources/views/content/lab2.blade_20210930155101.php
+++ .history/resources/views/content/lab2.blade_20210930155101.php
@@ -5,7 +5,5 @@
 <h1>Lab 2</h1>
     <p class="lead">This is the page for lab 2</p>
     <p class="lead">Click <a href="https://youtu.be/nDcP8n5fqac">here</a> for Part A.</p><br/>
-    <hr/>
-    <p class="lead">Click <a href="">here</a> for Part B.</p>
 
 @endsection
--- .history/resources/views/content/lab2.blade_20210930155128.php
+++ .history/resources/views/content/lab2.blade_20210930155128.php
@@ -6,6 +6,6 @@
     <p class="lead">This is the page for lab 2</p>
     <p class="lead">Click <a href="https://youtu.be/nDcP8n5fqac">here</a> for Part A.</p><br/>
     <hr/>
-    <p class="lead">Click <a href="">here</a> for Part B.</p>
+    <p class="lead">Click <a href="">here</a> </p>
 
 @endsection
